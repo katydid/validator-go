03959f0b5df5e86f43ce9f836ec9e7ebba44b94f
Walter Schulze <awalterschulze@gmail.com>
2024-10-19 18:33:33 +0100

    more protobuf cleanup

--- relapse/types/doc.go
+++ relapse/types/doc.go
@@ -1,3 +1,2 @@
-//Package types contains representations of the relapse types.
-//These types are specified as a protocol buffer enum so that they can be included in the expr ast protocol buffer specification.
+// Package types contains representations of the relapse types.
 package types
